69df0fce49be9c437d8248f55db2e68cc81823c9
LeoJian622 <hualuoyu_yks@qq.com>
2017-10-11 16:41:37 +0800

    eg 12 end    time:2017-10-11 16:41:34

--- 03struts_login/src/cn/sxt/action/AddAction.java
+++ 03struts_login/src/cn/sxt/action/AddAction.java
@@ -1,7 +1,15 @@
 package cn.sxt.action;
 
 public class AddAction {
-	public String execute() {
+	public String add() {
+		System.out.println("success");
+		return "success";
+	}
+	public String delete() {
+		System.out.println("success");
+		return "success";
+	}
+	public String update() {
 		System.out.println("success");
 		return "success";
 	}
